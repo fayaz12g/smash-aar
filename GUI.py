a9a9ad21f855d5aa56dc37e007c63f77f5a1ea4e
Fayaz Shaikh <61674751+fayaz12g@users.noreply.github.com>
2024-03-27 00:51:42 -0400

    icon update

--- GUI.py
+++ GUI.py
@@ -45,7 +45,7 @@ from compress import pack
 #### Create Window ####
 #######################
 
-tool_version = "2.0.0"
+tool_version = "2.0.1"
 
 root = customtkinter.CTk()
 root.title(f"Fayaz's Settings {tool_version} for Super Smash Brothers Ultimate 13.0.2")
